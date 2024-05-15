d1c51ea3093db518ed87046dfb0850949c5d0c5f
Jeromsundar <jeromsundar61@gmail.com>
2024-05-15 11:58:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dbConnect = require('./config/dbconnect');
+const app = express();
+const dotenv = require('dotenv').config();
+const authRouter = require("./routes/authRoute");
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 4000;
+
+dbConnect(); // Call dbConnect to establish database connection
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api/User", authRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
